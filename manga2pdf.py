490ebd5b932b56f71e4c9409e0d0d65c1834655b
Jerry Arciaga <jerryarciaga11@gmail.com>
2022-01-17 23:41:07 -0700

    download_image() function now converts downloaded image to pdf

--- manga2pdf.py
+++ manga2pdf.py
@@ -1,6 +1,5 @@
 import requests
 import img2pdf
-from PIL import Image
 import os
 
 #TODO Generate config file for these things
@@ -13,15 +12,19 @@ headers = {
 
 #TODO Get this url from parsed HTML
 url = 'https://cdn.readdetectiveconan.com/file/mangap/5624/10067000/1.jpeg'
-#TODO Add conversion to PDF functionality
 def download_image(url, filename):
-    with open(filename, "wb") as image:
+    img_filename = filename + ".jpeg"
+    pdf_filename = filename + ".pdf"
+    with open(img_filename, "wb") as image:
         r = requests.get(url, headers=headers)
         for chunk in r.iter_content(chunk_size=128):
             image.write(chunk)
+    with open(pdf_filename, "wb") as pdf:
+        pdf.write(img2pdf.convert(img_filename))
 
 
-download_image(url, "image.jpg") # Example usage
+
+download_image(url, "Teenage Mercenary") # Example usage
 
 #TODO Define functions to download from sources and turn into full PDF
 class Manga:
