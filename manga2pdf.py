d33de6305e9c1d861f9edf82141064cd3facc0cd
Jerry Arciaga <jerryarciaga11@gmail.com>
2022-02-24 12:23:02 +0900

    Refactored functions for readability

--- manga2pdf.py
+++ manga2pdf.py
@@ -13,32 +13,41 @@ headers = {
 
 # Download images, then convert to pdf
 def download_pdf(url, filename):
-    img_filename = filename + ".jpeg"
+    img_filename = filename + ".jpg"
     pdf_filename = filename + ".pdf"
+    params = {"v": "12"} # Specific to https://readm.org
+
     with open(img_filename, "wb") as image:
-        r = requests.get(url, headers=headers)
+        r = requests.get(url, headers=headers, params=params)}
         for chunk in r.iter_content(chunk_size=128):
             image.write(chunk)
     with open(pdf_filename, "wb") as pdf:
         pdf.write(img2pdf.convert(img_filename))
     os.remove(img_filename)
 
-# Example Usage
-if __name__ == '__main__':
-    numPages = 14 # TODO Get this number from parsing HTML
+# Uses download_pdf function to download a whole chapter
+def download_chapter(manga_id, chapter)
+    base_url = "https://www.readm.org/uploads/chapter_files/{manga_id}/{chapter}/"
+    current_page = 1
     chapter = PdfFileMerger(strict=False)
-    # TODO Turn this chunk of for loop code into a function
-    for page in range(numPages):
-        # TODO Scrape the two numbers from this url to specify chapter
-        url = f"https://cdn.readdetectiveconan.com/file/mangap/3262/10156000/{page+1}.jpeg"
-        download_pdf(url, f"Page {page + 1}")
-        currentFile = open(f"Page {page + 1}.pdf", "rb")
-        currentPage = PdfFileReader(currentFile)
-        chapter.merge(
-            position=page,
-            fileobj=currentPage,
-        )
-        currentFile.close()
-        os.remove(f"Page {page + 1}.pdf")
-    chapter.write('chapter.pdf')
+
+
+
+# Example Usage
+#if __name__ == '__main__':
+#    numPages = 10 # TODO Get this number from parsing HTML
+#    chapter = PdfFileMerger(strict=False)
+#    # TODO Turn this chunk of for loop code into a function
+#    for page in range(numPages):
+#        url = f"https://www.readm.org/uploads/chapter_files/17427/144/{page + 1}.jpg"
+#        download_pdf(url, f"Page {page + 1}")
+#        currentFile = open(f"Page {page + 1}.pdf", "rb")
+#        currentPage = PdfFileReader(currentFile)
+#        chapter.merge(
+#            position=page,
+#            fileobj=currentPage,
+#        )
+#        currentFile.close()
+#        os.remove(f"Page {page + 1}.pdf")
+#    chapter.write('chapter.pdf')
 
